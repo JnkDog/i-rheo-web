7f56f9c1d10d5327b7e3807ef52faa4082b061e2
yuyangovo <zhouccwinnie@163.com>
2021-07-14 20:56:55 +0100

    dip

--- algorithm/oversample.py
+++ algorithm/oversample.py
@@ -28,17 +28,17 @@ class oversampling:
         ginf = 0#delet after design input label
         InterpFunc = "slinear"#delet after design input label
         freqpoints = 200
-        DIP = math.log10(len(t))/math.log10(t[len(t)-1]/t[1]);
+        DIP = math.log10(len(t))/math.log10(t[-1]/t[0]);
         if DIP < 0.6:
-            OverSample = int(10^(math.log10(t[len(t)-1]/t[1])))
-            t_I = np.linspace(0,t[len(t)-1],OverSample)
+            OverSample = int(10^(math.log10(t[-1]/t[0])))
+            t_I = np.linspace(0,t[-1],OverSample)
             t1 = np.hstack((0,t))
             g1 = np.hstack((g0,g))
             f = interpolate.interp1d(t1,g1,InterpFunc)
             Gint_I=f(t_I)
         else:
             OverSample = int(len(t)*1e1)
-            t_I = np.linspace(0,t[len(t)-1],OverSample)
+            t_I = np.linspace(0,t[-1],OverSample)
             t1 = np.hstack((0,t))
             g1 = np.hstack((g0,g))
             f = interpolate.interp1d(t1,g1,"slinear")
