21d2ee9fc7a7c8b5d92ef8d8877eb52127431817
yuyangovo <zhouccwinnie@163.com>
2021-08-09 17:37:14 +0100

    bulk

---
+++ algorithm/bulk/stress.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+df_news = pd.read_table('PI130k-02_-30C_SR01_CP10H_gt.txt',sep='	',header = None)
+
+
+plt.plot(df_news[0],df_news[1],'-o', lw=3, color='royalblue',label='$G^{II}$')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Time(s)')
+plt.ylabel('Force(nN)')
+plt.show()
