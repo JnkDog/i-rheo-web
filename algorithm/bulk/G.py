9acfdbae0fddd060ba325c8d42ee0f5636338a62
wylwht2002 <wylwht2002@163.com>
2021-08-15 22:22:25 +0100

    更新

--- algorithm/bulk/G.py
+++ algorithm/bulk/G.py
@@ -14,35 +14,40 @@ ress = np.zeros(100, dtype=complex)
 resss = np.zeros(100, dtype=complex)
 ressss = np.zeros(100, dtype=complex)
 
-def manlio_ft(g, t, stress_0, stress_dot_inf, strain_0, strain_dot_inf, interpolate, oversampling, gg,ggg,  N_f = 100):
+def manlio_ft(g, t, stress_0, stress_dot_inf, strain_0, strain_dot_inf, interpolate, oversampling, gg,  N_f = 100):
 
     g = np.array(g)
     gg = np.array(gg)
-    ggg = np.array(ggg)
+    # ggg = np.array(ggg)
     t = np.array(t)
     if interpolate is True:
         gi = interp1d(t, g, kind='cubic', fill_value='extrapolate')
         ggi = interp1d(t, gg, kind='cubic', fill_value='extrapolate')
-        gggi = interp1d(t, ggg, kind='cubic', fill_value='extrapolate')
+        # gggi = interp1d(t, ggg, kind='cubic', fill_value='extrapolate')
         t_new = np.logspace(min(np.log10(t)), max(np.log10(t)), len(t) * oversampling)  # re-sample t in log space
         g = gi(t_new)  # get new g(t) taken at log-space sampled t
         gg = ggi(t_new)
-        ggg = gggi(t_new)
+        # ggg = gggi(t_new)
         t = t_new
+
+
+
     i = complex(0, 1)
     min_omega = 1 / max(t)
     max_omega = 1 / min(t)
     N_t = len(t)
     omega = np.logspace(np.log10(min_omega), np.log10(max_omega), N_f)
 
+    
+
+    zerooo = i * omega * strain_0 + (1 - np.exp(-i * omega * t[1])) * ((gg[1] - strain_0) / t[1]) \
+           + strain_dot_inf * np.exp(-i * omega * t[N_t - 1])
+    
     zero = i * omega * stress_0 + (1 - np.exp(-i * omega * t[1])) * ((g[1] - stress_0) / t[1]) \
            + stress_dot_inf * np.exp(-i * omega * t[N_t - 1])
 
-    zerooo = i * omega * strain_0 + (1 - np.exp(-i * omega * t[1])) * ((ggg[1] - strain_0) / t[1]) \
-           + strain_dot_inf * np.exp(-i * omega * t[N_t - 1])
-
     res= multiprocessing.Manager().list()
-    for xxx in range(100):
+    for xxx in range(100):  
         res.append(i)
     lock=multiprocessing.Manager().Lock()
     pool = multiprocessing.Pool(processes = 5)
@@ -57,30 +62,30 @@ def manlio_ft(g, t, stress_0, stress_dot_inf, strain_0, strain_dot_inf, interpol
     lock = multiprocessing.Manager().Lock()
     pool = multiprocessing.Pool(processes = 5)
     for q_i, q in enumerate(omega):
-        pool.apply_async(calcu, (N_t,gg,t,i,q,q_i,lock,zero,ress))   #维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
+        pool.apply_async(calcu, (N_t,gg,t,i,q,q_i,lock,zerooo,ress))   #维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
     pool.close()
     pool.join()
     
-    resss= multiprocessing.Manager().list()
-    for xxx in range(100):
-        resss.append(i)
-    lock = multiprocessing.Manager().Lock()
-    pool = multiprocessing.Pool(processes = 5)
-    for p_i, p in enumerate(omega):
-        pool.apply_async(calcu, (N_t,ggg,t,i,p,p_i,lock,zerooo,resss))   #维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
-    pool.close()
-    pool.join()
+    # resss= multiprocessing.Manager().list()
+    # for xxx in range(100):
+    #     resss.append(i)
+    # lock = multiprocessing.Manager().Lock()
+    # pool = multiprocessing.Pool(processes = 5)
+    # for p_i, p in enumerate(omega):
+    #     pool.apply_async(calcu, (N_t,ggg,t,i,p,p_i,lock,zerooo,resss))   #维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
+    # pool.close()
+    # pool.join()
 
-    ressss= multiprocessing.Manager().list()
-    for xxx in range(100):
-        ressss.append(i)
+    # ressss= multiprocessing.Manager().list()
+    # for xxx in range(100):
+    #     ressss.append(i)
     
-    lock = multiprocessing.Manager().Lock()
-    pool = multiprocessing.Pool(processes = 5)
-    for p_i, p in enumerate(omega):
-        pool.apply_async(calcu, (N_t,gg,t,i,p,p_i,lock,zerooo,ressss))   #维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
-    pool.close()
-    pool.join()
+    # lock = multiprocessing.Manager().Lock()
+    # pool = multiprocessing.Pool(processes = 5)
+    # for p_i, p in enumerate(omega):
+    #     pool.apply_async(calcu, (N_t,gg,t,i,p,p_i,lock,zerooo,ressss))   #维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
+    # pool.close()
+    # pool.join()
     
     # print("-----------------\n",res)
     # print(len(res))
@@ -88,7 +93,7 @@ def manlio_ft(g, t, stress_0, stress_dot_inf, strain_0, strain_dot_inf, interpol
     # print(len(ress))
 
 
-    return omega, ((res) / (i * omega) ** 2), ((ress) / (i * omega) ** 2), ((resss) / (i * omega) ** 2), ((ressss) / (i * omega) ** 2)
+    return omega, ((res) / (i * omega) ** 2), ((ress) / (i * omega) ** 2)
 
 def calcu(N_t,g,t,i,w,w_i,lock,zero,res):
 
@@ -107,24 +112,24 @@ def test(stress_0, stress_dot_inf,strain_0, strain_dot_inf,interpolate, oversamp
     funcc = df_news[2]
     func = df_news[1] 
     time = df_news[0]
-    func = func/funcc
-    funccc = df_news[1]
+    # func = func/funcc
+    # funccc = df_news[1]
 
-    omega, res_test, ress_test, resss_test, ressss_test = manlio_ft(func, time, stress_0, stress_dot_inf, strain_0, strain_dot_inf, interpolate, oversampling, funcc, funccc)
+    omega, res_test, ress_test = manlio_ft(func, time, stress_0, stress_dot_inf, strain_0, strain_dot_inf, interpolate, oversampling, funcc)
 
     G1=(res_test * omega * i)
     G2=(ress_test * omega * i)
-    G22=(ressss_test * omega * i)
-    G3=(resss_test * omega * i)
+    # G22=(ressss_test * omega * i)
+    # G3=(resss_test * omega * i)
     G4=(G1/G2)
-    G5=(G3/G22)
+    # G5=(G3/G22)
 
     plt.figure()
 
-    g_p = np.real(G5)
+    g_p = np.real(G4)
     g_pp = np.imag(G4)
-    plt.loglog(omega,g_p*10**5, '-o', lw=3, color='red',label='$G^{I}$') 
-    plt.loglog(omega, g_pp*10**6, '-o', lw=3, color='royalblue', label='$G^{II}$')
+    plt.loglog(omega,g_p, '-o', lw=3, color='red',label='$G^{I}$') 
+    plt.loglog(omega, g_pp, '-o', lw=3, color='royalblue', label='$G^{II}$')
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Moduli (Pa)')
     plt.legend()
@@ -134,7 +139,7 @@ def test(stress_0, stress_dot_inf,strain_0, strain_dot_inf,interpolate, oversamp
 if __name__ == '__main__':
     start=time.time()
     df_news = pd.read_table('PI130k-02_-30C_SR01_CP10H_gt.txt',sep='	',header = None)
-    test(stress_0=1, stress_dot_inf=0, strain_0=1, strain_dot_inf=0,interpolate=True, oversampling=10)
+    test(stress_0=0, stress_dot_inf=0, strain_0=0, strain_dot_inf=0, interpolate=False, oversampling=10)
     end=time.time()
     print("\ntime",end-start)
     
