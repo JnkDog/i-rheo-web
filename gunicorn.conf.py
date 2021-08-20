a0b67fdd0c69030e75f4975dca2e7bb109b9ee43
JnKDog <530661401@qq.com>
2021-08-20 09:08:13 +0100

    setting change

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,2 +1,15 @@
+# gunicorn.conf
+import logging
+import logging.handlers
+from logging.handlers import WatchedFileHandler
+import os
+import multiprocessing
+
+# gevent setting
+worker_class = 'gevent' 
+
+# max_processes
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = 2 
 graceful_timeout = 100
 timeout = 120
